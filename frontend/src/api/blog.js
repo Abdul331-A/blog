ff595d3f55db4c793737d44188a49ee1fbd8476d
muzthu47 <muhammedmusthafak100@gmail.com>
2025-08-06 18:16:24 +0530

    almost

---
+++ frontend/src/api/blog.js
@@ -0,0 +1,9 @@
+
+
+export async function getBlogs() {
+    const res = await fetch('http://localhost:3000/blogs');
+    const data = await res.json();
+    return data;
+    
+}
+// console.log(data);
--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { getBlogs } from "../api/blog";
 
 export default function Blogs() {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+  const load = async () => {
+   const data= await getBlogs()
+    setLoading(false);
+    setBlogs(data);
+    
+  }
+console.log({blogs});
+
+  useEffect(() => {
+    load();
+  }, []);
+  if (loading)
+    return <div>LOading</div>
   return (
     <div>
       <h1>Blogs</h1>
       <hr />
       <Link to='create/'>Create</Link>
       <ol>
-        <li>
-          <Link to='1/'>My First Blog</Link>
-        </li>
+        {blogs.map((Blogs) => (
+          <li>
+            <Link to='1/'>{Blogs.title}</Link>
+          </li>
+        ))}
       </ol>
     </div>
   );
