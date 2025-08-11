723d1b108b5c719bc441480c5a106698d1334391
Abdul331-A <abdulamcr7@gmail.com>
2025-08-11 12:13:25 +0530

    backend side setting

--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Blogs() {
+  const load= async ()=>{
+    const res=await fetch('http://localhost:3000/blogs/1');
+    const data=await res.json();
+    console.log(data);
+    
+  }
+  useEffect(()=>{
+    load();
+  },[]);
   return (
     <div>
       <h1>Blogs</h1>
